894f657cc252755c9a44e8df4f40b3f1cbf7754b
Chanelp <mariannissanchez27@gmail.com>
2023-01-30 14:59:39 -0400

    Protected service

--- Controllers/CategoriaContoller.cs
+++ Controllers/CategoriaContoller.cs
@@ -8,11 +8,11 @@ namespace WebApiNet.Controllers;
 public class CategoriaController : ControllerBase
 {
     // Recibir el servicio de Categoria
-    ICategoriaService categoriaService;
+    protected readonly ICategoriaService _categoriaService;
 
     public CategoriaController(CategoriaService service)
     {
-        categoriaService = service;
+        _categoriaService = service;
     }
 
     // Endpoints para devolver la información
@@ -20,27 +20,27 @@ public class CategoriaController : ControllerBase
     [HttpGet]
     public IActionResult Get()
     {
-        return Ok(categoriaService.Get());
+        return Ok(_categoriaService.Get());
     }
 
     [HttpPost]
     public IActionResult Post([FromBody] Categoria categoria)
     {
-        categoriaService.Save(categoria);
+        _categoriaService.Save(categoria);
         return Ok();
     }
 
     [HttpPut("{id}")]
     public IActionResult Put(Guid id, [FromBody] Categoria categoria)
     {
-        categoriaService.Update(id, categoria);
+        _categoriaService.Update(id, categoria);
         return Ok();
     }
 
     [HttpDelete("{id}")]
     public IActionResult Delete(Guid id)
     {
-        categoriaService.Delete(id);
+        _categoriaService.Delete(id);
         return Ok();
     }
 
